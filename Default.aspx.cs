80bed27e89e23abe672215efcdc6615294da3833
techstrikers <ajaysaptaputre@techstrikers.com>
2016-02-02 11:15:26 +0530

    CheckIn

---
+++ Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.IO;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.SendHtmlFormattedEmail("emailaddress@portal.com", "TestMail", this.PopulateBody());
+    }
+    private void SendHtmlFormattedEmail(string recepientEmail,string subject, string body)
+    {
+        using (MailMessage mailMessage = new MailMessage())
+        {
+            mailMessage.From = new MailAddress(ConfigurationManager.AppSettings["MailAddress"]);
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.To.Add(new MailAddress(recepientEmail));
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = ConfigurationManager.AppSettings["Host"]; 
+            System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+            NetworkCred.UserName = ConfigurationManager.AppSettings["UserName"]; 
+            NetworkCred.Password = ConfigurationManager.AppSettings["Password"];
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = NetworkCred;
+            smtp.Port = Convert.ToInt32(ConfigurationManager.AppSettings["Port"]);
+            smtp.Send(mailMessage);
+        }
+    }
+    private string PopulateBody()
+    {
+        string body = string.Empty;
+        using (StreamReader reader = new StreamReader(Server.MapPath("~/TestMail.htm")))
+        {
+            body = reader.ReadToEnd();
+        }
+       
+        return body;
+    }
+
+
+
+}
---
+++ image.ashx
@@ -0,0 +1,55 @@
+﻿<%@ WebHandler Language="C#" Class="image" %>
+ 
+using System;
+using System.Web;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Web.UI.WebControls;
+
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Net.Mail;
+using System.IO;
+
+public class image : IHttpHandler {
+
+    public void ProcessRequest(HttpContext context)
+    {
+        Bitmap bmp = new Bitmap(1, 1);
+        Graphics graphics = Graphics.FromImage(bmp);
+
+        int width = (int)Math.Ceiling(1.0);
+        int height = (int)Math.Ceiling(1.0);
+
+        bmp = new Bitmap(bmp, width, height);
+        graphics.Dispose();
+        graphics = Graphics.FromImage(bmp);
+        graphics.Clear(Color.White);
+
+        graphics.Dispose();
+        bmp.Save(context.Response.OutputStream, ImageFormat.Gif);
+
+        string ip_address = "";
+        if (string.IsNullOrEmpty(HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]))
+        {
+            ip_address = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+        }
+        else
+        {
+            ip_address = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"].Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
+        }
+
+        using (System.IO.StreamWriter writer = new System.IO.StreamWriter(context.Server.MapPath("IPAddress.txt"), true))
+        {
+            writer.WriteLine(ip_address);
+        }
+        
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+ 
+}
